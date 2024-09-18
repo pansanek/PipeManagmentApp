9aa1f662718db83dd69ce4c3cb31512b4b3e1058
SashaPotemkin <alex.potjomkin@gmail.com>
2024-09-18 14:37:51 +0300

    Добавьте файлы проекта.

---
+++ PipeManagmentApp/Startup.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+
+namespace PipeManagment
+{
+    public class Startup
+    {
+        
+        public void ConfigureServices(IServiceCollection services)
+        {
+           
+            services.AddMvc(options => options.EnableEndpointRouting = false);
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+            app.UseSession();
+            app.UseMvcWithDefaultRoute();
+           
+        }
+    }
+}
